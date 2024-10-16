34b31e4246bd989bbbd318881f4485ef061c4444
jordankanghm <jordankanghm@gmail.com>
2024-10-16 16:01:26 +0800

    Sync backend for request creation and reading

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,18 @@ app.use(express.json());
 const authRoutes = require('./routes/auth'); // Import your authentication routes
 app.use('/api/auth', authRoutes); // API route for authentication
 
+const reqRoutes = require('./routes/requests');
+app.use('/api/requests', reqRoutes);
+
+// Sync database model
+sequelize.sync({ alter: true }) // This will create/update tables if they exist
+  .then(() => {
+    console.log('Database synced');
+  })
+  .catch(err => {
+    console.error('Error syncing database:', err);
+  });
+
 // Start the server
 const PORT = process.env.PORT || 3000; // Use port from environment or default to 3000
 app.listen(PORT, () => {
